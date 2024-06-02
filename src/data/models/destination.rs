7634b01d8d3258aedeb2931e965cdf9a916c3b05
Maxime-Bolduc <max.xam3000@gmail.com>
2024-06-01 21:56:30 -0400

    Add http auth for destinations

--- src/data/models/destination.rs
+++ src/data/models/destination.rs
@@ -1,24 +1,44 @@
 use std::collections::HashMap;
 use rocket::serde::{Deserialize, Serialize};
+use sqlx::postgres::PgRow;
 use sqlx::{
   PgConnection,
   prelude::FromRow,
   types::Json,
+  Row,
 };
 
 use crate::data::Error;
 use crate::data::Queryable;
+use crate::data::models::AuthType;
 
 use super::Source;
 
-#[derive(Serialize, Deserialize, FromRow, Default)]
+#[derive(Serialize, Deserialize)]
 pub struct Destination {
   pub id: Option<i32>,
   pub path: String,
   pub protocol: String,
-  #[sqlx(json)]
   pub headers: HashMap<String, String>,
-  pub filter: Option<String>
+  pub filter: Option<String>,
+  pub auth: AuthType,
+}
+
+impl FromRow<'_, PgRow> for Destination {
+  fn from_row(row: &'_ PgRow) -> Result<Self, sqlx::Error> {
+    Ok(Self {
+      id: row.try_get("id")?,
+      path: row.try_get("path")?,
+      protocol: row.try_get("protocol")?,
+      headers: row.try_get::<Json<HashMap<String, String>>, _>("headers")?.0,
+      filter: row.try_get("filter")?,
+      auth: match row.try_get_unchecked("auth_type")? {
+        "basic" => AuthType::Basic { username: row.try_get("auth_username")?, password: row.try_get("auth_password")? },
+        "bearer" => AuthType::Bearer { token: row.try_get("auth_token")? },
+        "none" | _ => AuthType::None,
+      },
+    })
+  }
 }
 
 impl Destination {
@@ -28,7 +48,11 @@ impl Destination {
                destinations.path,
                destinations.protocol,
                destinations.headers,
-               destinations.filter
+               destinations.filter,
+               destinations.auth_type,
+               destinations.auth_username,
+               destinations.auth_password,
+               destinations.auth_token
         FROM destinations
         WHERE destinations.path = $1
       ")
@@ -68,7 +92,11 @@ impl Queryable for Destination {
                destinations.path,
                destinations.protocol,
                destinations.headers,
-               destinations.filter
+               destinations.filter,
+               destinations.auth_type,
+               destinations.auth_username,
+               destinations.auth_password,
+               destinations.auth_token
         FROM destinations
         WHERE destinations.id = $1
       ")
@@ -79,13 +107,17 @@ impl Queryable for Destination {
 
   async fn insert(&self, conn: &mut PgConnection) -> Result<(), Error> {
     sqlx::query("
-        INSERT INTO destinations (path, protocol, headers, filter)
-        VALUES ($1, $2, $3, $4)
+        INSERT INTO destinations (path, protocol, headers, filter, auth_type, auth_username, auth_password, auth_token)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
       ")
       .bind(&self.path)
       .bind(&self.protocol)
       .bind(Json(&self.headers))
       .bind(&self.filter)
+      .bind(&self.auth.to_string())
+      .bind(&self.auth.username())
+      .bind(&self.auth.password())
+      .bind(&self.auth.token())
       .execute(conn)
       .await?;
 
@@ -97,12 +129,20 @@ impl Queryable for Destination {
         UPDATE destinations
         SET protocol = $1,
             headers = $2,
-            filter = $3
-        WHERE destinations.path = $4
+            filter = $3,
+            auth_type = $4
+            auth_username = $5,
+            auth_password = $6,
+            auth_token = $7
+        WHERE destinations.path = $8
       ")
       .bind(&self.protocol)
       .bind(Json(&self.headers))
       .bind(&self.filter)
+      .bind(&self.auth.to_string())
+      .bind(&self.auth.username())
+      .bind(&self.auth.password())
+      .bind(&self.auth.token())
       .bind(&self.path)
       .execute(conn)
       .await?;
