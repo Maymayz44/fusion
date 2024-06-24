858064ff9f787710eae3b3c872bb53cf2e80147f
Maxime-Bolduc <max.xam3000@gmail.com>
2024-06-23 20:08:07 -0400

    Isolate fusion server into a thread

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use api::Bindings;
+use api::FusionConfig;
+use crate::admin::{App, file_and_error_handler};
 use dotenv::dotenv;
 use axum::{
   routing::get, Router
 };
 use std::error::Error;
+use tokio::task;
 
 pub mod api;
 pub mod data;
@@ -11,14 +13,20 @@ pub mod data;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
   dotenv()?;
-  let bindings = Bindings::env();
   data::init_pool().await?;
 
-  let app = Router::new()
-    .route(format!("{}/*path", &bindings.path).as_str(), get(api::entrypoint));
+  let fusion_server = task::spawn(async move {
+    let fusion_config = FusionConfig::env();
 
-  let listener = tokio::net::TcpListener::bind(bindings.full_address()).await.unwrap();
-  axum::serve(listener, app).await?;
+    let fusion_router = Router::new()
+      .route(format!("{}/*path", &fusion_config.path).as_str(), get(api::entrypoint));
+    let fusion_listener = tokio::net::TcpListener::bind(fusion_config.full_address()).await.unwrap();
+    
+    println!("Fusion server running on http://{}:{}{}", &fusion_config.address, &fusion_config.port, &fusion_config.path);
+    axum::serve(fusion_listener, fusion_router).await.unwrap();
+  });
+
+  fusion_server.await.unwrap();
 
   Ok(())
 }
